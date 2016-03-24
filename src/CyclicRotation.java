db9099110d2b9f77f14966ff0018c6155b64b8b2
Vishwa Nawarathne <vishwan@VISHWAN-BP.local>
2016-03-24 12:57:42 +0530

    handled checking empty array

--- src/CyclicRotation.java
+++ src/CyclicRotation.java
@@ -24,20 +24,20 @@ public class CyclicRotation {
 	        // write your code in Java SE 8
 	        
 	        int[] last = A;
-	        for (int i = 0; i < K; i++) {
-	        	int lastNum = last[last.length-1];
-	        	
-	        	int tempFirstInLast = last[0];
-	        		        	
-	        	for (int j = last.length-2; j >= 0; j--) {
-					last[j+1] = last[j];
-				}
-	        	
-	        	last[0] = lastNum;
-			}
-	        
+	        if(last.length>0){
+	        	 for (int i = 0; i < K; i++) {
+	 	        	int lastNum = last[last.length-1];
+	 	        	
+	 	        	int tempFirstInLast = last[0];
+	 	        		        	
+	 	        	for (int j = last.length-2; j >= 0; j--) {
+	 					last[j+1] = last[j];
+	 				}
+	 	        	
+	 	        	last[0] = lastNum;
+	 			}
+	        }
 	        return last;
-	      
 	    }
 
 }
